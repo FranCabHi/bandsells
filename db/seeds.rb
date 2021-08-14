e7c6064808bf529c80bff6ce81e197d312c72090
Fran Cabrera <f.cabrerahiche@yahoo.es>
2021-08-13 21:31:59 -0400

    Adds mockup images for products - Changes product controller to link created products to current user - Amends product form to remove field for user - Removes quantity field from product show - Amends product image link in productlist - Adds seed for product

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,45 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.delete_all
-code = "password"
-50.times do 
-    User.create(
-        email: "owner_"+"#{rand(1..50)}"+"@example.com",
-        name: Faker::Music.unique.band,
-        country: Faker::Address.country,
-        password: code
-    ).add_role :owner
-end
+Product.delete_all
+# code = "password"
+# 50.times do 
+#     User.create(
+#         email: "owner_"+"#{rand(1..50)}"+"@example.com",
+#         name: Faker::Music.unique.band,
+#         country: Faker::Address.country,
+#         password: code
+#     ).add_role :owner
+# end
 
-50.times do 
-    User.create(
-        email: "user_"+"#{rand(1..50)}"+"@example.com",
-        name: Faker::Name.unique.name,
-        country: Faker::Address.country,
-        password: code
-    ).add_role :normaluser
-end
+# 50.times do 
+#     User.create(
+#         email: "user_"+"#{rand(1..50)}"+"@example.com",
+#         name: Faker::Name.unique.name,
+#         country: Faker::Address.country,
+#         password: code
+#     ).add_role :normaluser
+# end
 
-1.times do 
-    User.create(
-        email: "admin@example.com", 
-        name: "admin", 
-        country: "Chile", 
-        password: code
-    ).add_role :admin
+# 1.times do 
+#     User.create(
+#         email: "admin@example.com", 
+#         name: "admin", 
+#         country: "Chile", 
+#         password: code
+#     ).add_role :admin
+# end
+
+users = User.with_role(:owner).pluck(:id)
+
+50.times do  
+    good = Product.create(
+        title: Faker::Music.album,
+        description: Faker::Lorem.sentence(word_count: 5),
+        stock: 50,
+        unit_price: rand(1199..2099)*10,
+        user_id: users.sample
+    )
+    random = rand(1..9)
+    good.image.attach(io: File.open("app/assets/images/00"+"#{random}"+".jpg"), filename: "00"+"#{random}"+".jpg", content_type: 'image/jpg')
 end
