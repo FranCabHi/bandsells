d43748ba61742d52b4aaffac4d8f19c315b8456a
Fran Cabrera <f.cabrerahiche@yahoo.es>
2021-08-14 19:36:37 -0400

    Adds validations to product model and removes notices from views

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 class Product < ApplicationRecord
+  resourcify
   belongs_to :user
+  has_many :product_orders
   has_many :orders, through: :product_orders
   has_one_attached :image
-  resourcify
+  
+  validates :title, :stock, :unit_price, presence: true
+  validates :description, length: {maximum: 500, too_long: "%#{count} es el límite de caracteres."}
+  validates :title, length: {maximum: 140, too_long: "%#{count} es el límite de caracteres."}
+  validates :price, numericality: {only_integer: true}
 end
