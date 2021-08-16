ef2fcb7484a4604a11a0e6cde15257e7c75b9587
Fran Cabrera <f.cabrerahiche@yahoo.es>
2021-08-15 20:26:51 -0400

    Adds total, subtotal and price methods to models

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,18 @@ class Order < ApplicationRecord
   has_many :products, through: :product_orders
   has_one :payment
   resourcify
+  before_save :set_subtotal
+
+  def subtotal
+    product_orders.collect{|product_order| product_order.valid? ? product_order.unit_price*product_order.quantity : 0}.sum
+end
+
+
+
+private
+
+def set_subtotal
+    self[:subtotal] = subtotal
+end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,8 @@ class Product < ApplicationRecord
   has_one_attached :image
   
   validates :title, :stock, :unit_price, presence: true
-  validates :description, length: {maximum: 500, too_long: "%#{count} es el límite de caracteres."}
-  validates :title, length: {maximum: 140, too_long: "%#{count} es el límite de caracteres."}
+  validates :description, length: {maximum: 500, too_long: "%#{count} characters max allowed."}
+  validates :title, length: {maximum: 140, too_long: "%#{count} characters max allowed."}
+
+
 end
