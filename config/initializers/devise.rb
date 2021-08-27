744409b3e500152101c252de587871cc85e18dee
Fran Cabrera <f.cabrerahiche@yahoo.es>
2021-08-26 20:59:40 -0400

    changing credentials to ENV

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, Rails.application.credentials.dig(:facebook, :app_id), Rails.application.credentials.dig(:facebook,:app_secret), token: {parse: :json}
+  config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], token: {parse: :json}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
