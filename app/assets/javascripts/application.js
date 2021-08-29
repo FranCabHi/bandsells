1e97c7bb2ef988ec9e9d79e55170f59738ab78db
Fran Cabrera <f.cabrerahiche@yahoo.es>
2021-08-28 22:01:08 -0400

    removes js from application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,3 @@
  * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
  */
 // Highlights current date on contact page
-window.addEventListener("DOMContentLoaded", (event) => {
-  const listHoursArray = document.body.querySelectorAll(".list-hours li");
-  listHoursArray[new Date().getDay()].classList.add("today");
-});
