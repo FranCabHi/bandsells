77260375c3bfaf1b54f3b832c4f79908203ada4a
Fran Cabrera <f.cabrerahiche@yahoo.es>
2021-09-10 20:56:40 -0300

    Adds more info to readme file

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # README
 
 - Trello: https://trello.com/invite/b/hFaB2BWz/d74119bb37338968717475d52a334bff/proyecto-final
-- Dominio: http://bandsells.xyz/
-- Heroku: https://afternoon-harbor-73328.herokuapp.com/
+- Dominio: http://bandsells.xyz/ (sin certificado)
+- Heroku: https://afternoon-harbor-73328.herokuapp.com/ (con certificado)
 
 ### Sobre la aplicación
 
@@ -11,6 +11,25 @@ Esta es una pequeña aplicación de ecommerce con 3 niveles de usuarios:
 - administrador
 - vendedor
 - usuario regular
+
+### Deployment
+
+1. Requisitos: ruby '2.5.3' y 'rails', '~> 5.2.6'
+2. Descargar la aplicación
+3. Migrar la base de datos y cargar las gemas enlazadas
+4. Cargar el seed para incluir usuarios, compras realizadas, productos y pagos
+5. Si se accede desde la URL hay 3 credenciales disponibles:
+
+- usuario: admin@example.com
+- contraseña: password
+
+- usuario: owner32@example.com
+- contraseña: password
+
+- usuario: user12@example.com
+- contraseña: password
+
+- También está disponible el acceso con FB, pero hay que ingresar con el sitio con certificado (Heroku). Los usuarios nuevos tienen acceso como usuario regular, pero el usuario admin puede dar más permisos desde la ventana de usuarios (acceso desde Dashboard haciendo clic en la cantidad de usuarios registrados)
 
 ### Integración
 
@@ -22,15 +41,9 @@ La aplicación cuenta con la opción de iniciar sesión desde Facebook y los pag
 
 El administrador tiene acceso especial a un dashboard completo con información de usuarios, productos disponibles y pedidos en curso. Puede gestionar todos estas secciones y otorgar permisos a los usuarios regulares para vender productos.
 
-usuario: admin@example.com
-contraseña: password
-
 #### Vendedor
 
 El vendedor puede comprar y vender productos. También tiene acceso a un dashboard reducido sobre el rendimiento de sus productos, así como la posibilidad de editar la información de los productos que ofrece actualmente, incluido el stock disponible.
-
-usuario: owner32@example.com
-contraseña: password
 
 #### Usuario regular
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,84 +11,86 @@
 # Order.delete_all
 # Payment.delete_all
 
-# code = "password"
+code = "password"
 
-# 1.times do 
-#   User.create(
-#       email: "admin@example.com", 
-#       name: "admin", 
-#       country: "Chile", 
-#       password: "password"
-#   ).add_role :admin
-# end
+1.times do 
+  a = User.create(
+      email: "admin@example.com", 
+      name: "admin", 
+      country: "Chile", 
+      password: "password"
+  )
+  a.add_role :admin
+  a.remove_role :normaluser
+end
 
-# 50.times do 
-#   a = User.create(
-#       email: "owner#{rand(100..200)}@example.com",
-#       name: Faker::Artist.unique.name,
-#       country: Faker::Address.country,
-#       password: code)
-#   a.add_role :owner
-#   a.remove_role :normaluser
-# end
+50.times do 
+  a = User.create(
+      email: "owner#{rand(100..200)}@example.com",
+      name: Faker::Artist.unique.name,
+      country: Faker::Address.country,
+      password: code)
+  a.add_role :owner
+  a.remove_role :normaluser
+end
 
-# 50.times do 
-#   User.create(
-#       email: "user#{rand(100..200)}@example.com",
-#       name: Faker::Name.unique.name,
-#       country: Faker::Address.country,
-#       password: code)
-# end
+50.times do 
+  User.create(
+      email: "user#{rand(100..200)}@example.com",
+      name: Faker::Name.unique.name,
+      country: Faker::Address.country,
+      password: code)
+end
 
 
-# users = User.with_role(:owner).pluck(:id)
+users = User.with_role(:owner).pluck(:id)
 
-# 60.times do  
-#   good = Product.create(
-#       title: Faker::Music.unique.album,
-#       description: Faker::Lorem.sentence(word_count: 5),
-#       stock: rand(1..20),
-#       unit_price: rand(1199..2099)*10,
-#       user_id: users.sample
-#   )
-#   random = rand(1..10)
-#   good.image.attach(io: File.open("app/assets/images/00"+"#{random}"+".jpg"), filename: "00"+"#{random}"+".jpg", content_type: 'image/jpg')
-# end
+60.times do  
+  good = Product.create(
+      title: Faker::Music.unique.album,
+      description: Faker::Lorem.sentence(word_count: 5),
+      stock: rand(1..20),
+      unit_price: rand(1199..2099)*10,
+      user_id: users.sample
+  )
+  random = rand(1..10)
+  good.image.attach(io: File.open("app/assets/images/00"+"#{random}"+".jpg"), filename: "00"+"#{random}"+".jpg", content_type: 'image/jpg')
+end
 
-# products = Product.all.pluck(:id, :unit_price)
-# u_normal = User.with_role(:normaluser).pluck(:id)
+products = Product.all.pluck(:id, :unit_price)
+u_normal = User.with_role(:normaluser).pluck(:id)
 
-# 100.times do
-#   product = products.sample
-#   productid = product[0]
-#   productprice = product[1]
-#   date = Faker::Date.between(from: '2018-01-01', to: Date.today)
+100.times do
+  product = products.sample
+  productid = product[0]
+  productprice = product[1]
+  date = Faker::Date.between(from: '2018-01-01', to: Date.today)
 
-#   order = Order.create(
-#     quantity: 1,
-#     total: productprice,
-#     state: 2,
-#     user_id: u_normal.sample,
-#     created_at: date,
-#     updated_at: date,
-#   )
+  order = Order.create(
+    quantity: 1,
+    total: productprice,
+    state: 2,
+    user_id: u_normal.sample,
+    created_at: date,
+    updated_at: date,
+  )
 
-#   item = ProductOrder.create(
-#     product_id: productid,
-#     quantity: 1,
-#     price: productprice,
-#     order_id: order.id
-#   )
+  item = ProductOrder.create(
+    product_id: productid,
+    quantity: 1,
+    price: productprice,
+    order_id: order.id
+  )
 
-#   paymentcreate = Payment.create(
-#   status: 0,
-#   total: productprice,
-#   merchant_order_id: "3174290905",
-#   order_id: order.id,
-#   created_at: date,
-#   updated_at: date,
-#   )
+  paymentcreate = Payment.create(
+  status: 0,
+  total: productprice,
+  merchant_order_id: "3174290905",
+  order_id: order.id,
+  created_at: date,
+  updated_at: date,
+  )
 
-# end
+end
 
-# puts "done"
+puts "done"
