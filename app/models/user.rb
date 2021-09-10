ec410444d156d3f465fbb6672a2fe53a3a937b6c
Fran Cabrera <f.cabrerahiche@yahoo.es>
2021-09-09 22:49:45 -0300

    minor change in payment association

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable
   
   has_many :products, dependent: :destroy
-  has_many :orders, dependent: :destroy
+  has_many :orders, dependent: :nullify
   acts_as_voter
 
   def assign_default_role
